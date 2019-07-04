d9eb572f8c7f07da3d60105ca41274aaa6bb247e
Rodrigo Orozco <rod.mx90@gmail.com>
2019-07-04 14:29:13 +0200

    buger menu

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,11 +1,28 @@
 $(document).ready(function() {
-    //SCROLL MENU
-    $(window).on('scroll', function(){
-       if($(window).scrollTop()> 60){
-           $('.menu .row').addClass('scroll-menu');
-       }
-       else{
-           $('.menu .row').removeClass('scroll-menu');
-       }
-    })
+    //BURGER
+    $('.burger').click(function(){
+        $(this).toggleClass('open');
+        $('nav').toggleClass('showNav');
+    });
+
+    if ($(window).width() < 992) {
+      // BELOW TO 992px
+      $( "nav > ul > li > a" ).click(function(e) {
+        e.preventDefault();
+        $(this).next($('.submenu')).toggleClass('submenu-open');
+      });
+
+    }
+    else {
+        // UP TO 992px
+        //SCROLL MENU
+        $(window).on('scroll', function(){
+           if($(window).scrollTop()> 60){
+               $('.menu .row').addClass('scroll-menu');
+           }
+           else{
+               $('.menu .row').removeClass('scroll-menu');
+           }
+       });
+    }
 });
