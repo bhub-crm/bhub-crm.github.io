5eaa97b564dc9ab1ad289154eb412c0f6c24594b
sitan.zewde <sitanzewde@Sitans-MacBook-Pro.local>
2019-07-08 15:06:10 +0200

    Sitan | responsive design for fetures

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -5,24 +5,72 @@ $(document).ready(function() {
         $('nav').toggleClass('showNav');
     });
 
-    if ($(window).width() < 992) {
-      // BELOW TO 992px
-      $( "nav > ul > li > a" ).click(function() {
-        //e.preventDefault();
+    // if ($(window).width() < 992) {
+    //   // BELOW TO 992px
+    //   $( "nav > ul > li > a" ).click(function() {
+    //     // e.preventDefault();
+    //     $(this).next($('.submenu')).toggleClass('submenu-open');
+    //   });
+
+    // }
+    // else {
+    //     // UP TO 992px
+    //     //SCROLL MENU
+    //     $(window).on('scroll', function(){
+    //        if($(window).scrollTop()> 60){
+    //            $('.menu .row').addClass('scroll-menu');
+    //        }
+    //        else{
+    //            $('.menu .row').removeClass('scroll-menu');
+    //        }
+    //    });
+    // }
+});
+
+
+const button = document.getElementById('.submenu').addEventListener('click', resize);
+
+function resize () {
+
+if($(window).width() < 992) {
+    // BELOW TO 992px
+    $( "nav > ul > li > a" ).click(function() {
+        // e.preventDefault();
         $(this).next($('.submenu')).toggleClass('submenu-open');
-      });
+    });
 
     }
     else {
         // UP TO 992px
         //SCROLL MENU
         $(window).on('scroll', function(){
-           if($(window).scrollTop()> 60){
-               $('.menu .row').addClass('scroll-menu');
-           }
-           else{
-               $('.menu .row').removeClass('scroll-menu');
-           }
-       });
+            if($(window).scrollTop()> 60){
+                $('.menu .row').addClass('scroll-menu');
+            }
+            else{
+                $('.menu .row').removeClass('scroll-menu');
+            }
+        });
     }
-});
+    
+}
+// if($(window).width() < 992) {
+//     // BELOW TO 992px
+//     $( "nav > ul > li > a" ).click(function() {
+//       // e.preventDefault();
+//       $(this).next($('.submenu')).toggleClass('submenu-open');
+//     });
+
+//   }
+//   else {
+//       // UP TO 992px
+//       //SCROLL MENU
+//       $(window).on('scroll', function(){
+//          if($(window).scrollTop()> 60){
+//              $('.menu .row').addClass('scroll-menu');
+//          }
+//          else{
+//              $('.menu .row').removeClass('scroll-menu');
+//          }
+//      });
+//   }
